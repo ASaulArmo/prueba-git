65dc78a1d29d0b4a11b2b4f7d8645dbe89dc40ea
Alberto Armendariz <saul_armo@hotmail.com>
2017-03-22 17:59:09 -0600

    Initial Commit

---
+++ Prueba GIT01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba GIT01
+//
+//  Created by Alberto S Armendariz on 3/22/17.
+//  Copyright © 2017 Alberto S Armendariz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prueba GIT01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Prueba GIT01
+//
+//  Created by Alberto S Armendariz on 3/22/17.
+//  Copyright © 2017 Alberto S Armendariz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
